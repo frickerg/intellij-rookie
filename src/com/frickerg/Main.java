15da09f49f79206e9f1a61fc5fa8f32da5a0f333
Guillaume Fricker <guillaume.fricker@cistec.com>
2020-10-05 16:57:25 +0200

    Search for Text using eclipse keymap with Ctrl+H

--- src/com/frickerg/Main.java
+++ src/com/frickerg/Main.java
@@ -4,12 +4,12 @@ import com.frickerg.model.Product;
 
 public class Main {
 
-    public static void main(String[] args) {
-        final String helloWorld = "Hello World";
-        System.out.println(helloWorld);
+	public static void main(String[] args) {
+		final String helloWorld = "Hello World";
+		System.out.println(helloWorld);
 
-        final String testProduct = "Test Product";
-        final Product product = new Product(testProduct);
-        System.out.println(product.getName());
-    }
+		final String testProduct = "Test Product";
+		final Product product = new Product(testProduct);
+		System.out.println(product.getName());
+	}
 }
