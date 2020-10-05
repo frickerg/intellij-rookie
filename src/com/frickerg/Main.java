e4f3592d2248b15a37f81e79045aca79957293ff
Guillaume Fricker <guillaume.fricker@cistec.com>
2020-10-05 15:42:44 +0200

    Created Product class to test auto completion class and get

--- src/com/frickerg/Main.java
+++ src/com/frickerg/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        final Product product = new Product("Test Product");
+        System.out.println(product.getName());
     }
 }
