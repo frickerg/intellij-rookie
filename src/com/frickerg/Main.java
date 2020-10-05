ae31a408350f35b5b4d866336443a3dfa31ab35a
Guillaume Fricker <guillaume.fricker@cistec.com>
2020-10-05 16:54:13 +0200

    Refactored variable using Alt+Shift+R

--- src/com/frickerg/Main.java
+++ src/com/frickerg/Main.java
@@ -4,10 +4,9 @@ import com.frickerg.model.Product;
 
 public class Main {
 
-    private static String HELLO_WORLD = "Hello World";
-
     public static void main(String[] args) {
-        System.out.println(HELLO_WORLD);
+        final String helloWorld = "Hello World";
+        System.out.println(helloWorld);
 
         final String testProduct = "Test Product";
         final Product product = new Product(testProduct);
