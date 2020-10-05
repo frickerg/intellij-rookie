b1e6b8557ab5e05241d08b2ab4c8bdb84ce52bb2
Guillaume Fricker <guillaume.fricker@cistec.com>
2020-10-05 16:36:05 +0200

    Tried refactoring variable with Ctrl+Alt+V

--- src/com/frickerg/Main.java
+++ src/com/frickerg/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        final Product product = new Product("Test Product");
+        final String test_product = "Test Product";
+        final Product product = new Product(test_product);
         System.out.println(product.getName());
     }
 }
