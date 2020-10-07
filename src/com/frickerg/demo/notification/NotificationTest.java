2525bb95478dd0119de0f562dd599fece8126bed
Guillaume Fricker <guillaume.fricker@cistec.com>
2020-10-07 10:04:40 +0200

    Generated Unit Test with Alt+Insert

---
+++ src/com/frickerg/demo/notification/NotificationTest.java
@@ -0,0 +1,35 @@
+package com.frickerg.demo.notification;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NotificationTest {
+
+	@Before public void setUp() throws Exception {
+	}
+
+	@After public void tearDown() throws Exception {
+	}
+
+	@Test public void getMessage() {
+	}
+
+	@Test public void setMessage() {
+	}
+
+	@Test public void getTimestamp() {
+	}
+
+	@Test public void setTimestamp() {
+	}
+
+	@Test public void testToString() {
+	}
+
+	@Test public void testEquals() {
+	}
+
+	@Test public void testHashCode() {
+	}
+}
